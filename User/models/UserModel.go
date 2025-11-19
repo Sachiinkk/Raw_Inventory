508b1efd6cb8cf3dce9346cb7820dea390d56208
Sachin kumar Saroj <sachinkumar9721830891@gmail.com>
2025-11-19 16:49:38 +0530

    updated database Schema

--- User/models/UserModel.go
+++ User/models/UserModel.go
@@ -4,9 +4,9 @@ import "database/sql"
 
 type User struct {
 	Id       int    `json:"id"`
-	Username string `json:"username"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	Username string `json:"username" validator:"require , min =3"`
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min =6"`
 }
 
 func CreateTable(db *sql.DB) error {
@@ -15,8 +15,11 @@ func CreateTable(db *sql.DB) error {
 		id INT AUTO_INCREMENT PRIMARY KEY,
 		username VARCHAR(255) NOT NULL,
 		email VARCHAR(255) NOT NULL UNIQUE,
+
 		password VARCHAR(255) NOT NULL,
-		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+		role VARCHAR(255) NOT NULL,
+		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+		updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 	);
 	`
 	_, err := db.Exec(query)
